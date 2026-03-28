bda01558909eed9ed1ecddc74cf2e0ff910906c6
juandiegodelossantos-coder <juan_diego_delossantos@dlsu.edu.ph>
2026-03-28 16:54:06 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,39 @@ int main(void) {
     // Example flow:
     //   1. Read a position (a, b) from the user.
     //   2. Call NextPlayerMove(&gs, pos).
-    //   3. Check gs.over; if true, call GameOver(&gs) and print the result.
+    //   3. Check gs.over; if true, call GameOver(&gs) and print the results
+
+Pos p;
+
+while (!gs.over){
+	
+	printf("\nCurrent Turn: %s\n", gs.go ? "R" : "B");
+	
+	printf("Enter row (1-3):");
+	scanf("%d", &p.a);
+	
+	
+	printf("Enter column (1-3): ");
+	scanf("%d", &p.b);
+	
+	if(inM(p)) {
+		NextPlayerMove(&gs, p);
+				if(!gs.good && !gs.over){
+					printf("Invalid move! Try Again.\n");
+								
+				}
+	} 
+	
+	else {
+		printf("Invalid position! TRY AGAIN.\n");
+		
+	}	
+		
+	}
+	
+	printf("\nGame Over!\n");
+	printf("%s\n", GameOver(&gs));
+
 
     return 0;
 }
