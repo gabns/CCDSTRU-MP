124db1db96a75fde57d6c743a9f16cb562422139
juandiegodelossantos-coder <juan_diego_delossantos@dlsu.edu.ph>
2026-03-28 13:38:26 +0800

    Updated Loops

--- main.c
+++ main.c
@@ -11,5 +11,31 @@ int main(void) {
     //   2. Call NextPlayerMove(&gs, pos).
     //   3. Check gs.over; if true, call GameOver(&gs) and print the result.
 
+Pos p;
+
+while (!gs.over){
+	
+	printf("\nCurrent Turn: %s\n", gs.go ? "R" : "B");
+	
+	printf("Enter row (1-3):");
+	scanf("%d", &p.a);
+	
+	printf("Enter column (1-3): ");
+	scanf("%d", &p.b);
+	
+	if(inM(p)) {
+		NextPlayerMove(&gs, p);
+				if(!gs.good && !gs.over){
+					printf("Invalid move! Try Again.\n");								
+				}
+	} 
+	else {
+		printf("Invalid position! TRY AGAIN.\n");	
+	}		
+	}
+	
+	printf("\nGame Over!\n");
+	printf("%s\n", GameOver(&gs));
+    
     return 0;
 }
